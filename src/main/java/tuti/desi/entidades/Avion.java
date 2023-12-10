de5e55c0b322ae3a6aef0b259c81c09957852af7
webJaviC <javicairoli3@gmail.com>
2023-12-10 15:34:36 -0300

    clases avion y vuelo terminadas

--- src/main/java/tuti/desi/entidades/Avion.java
+++ src/main/java/tuti/desi/entidades/Avion.java
@@ -1,5 +1,41 @@
 package tuti.desi.entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Avion {
+
+	@Id
+	private Long nroAvion;
+	
+	private String cantFilas;
+	
+	private String cantAsientos;
+
+	public Long getNroAvion() {
+		return nroAvion;
+	}
+
+	public void setNroAvion(Long nroAvion) {
+		this.nroAvion = nroAvion;
+	}
+
+	public String getCantFilas() {
+		return cantFilas;
+	}
+
+	public void setCantFilas(String cantFilas) {
+		this.cantFilas = cantFilas;
+	}
+
+	public String getCantAsientos() {
+		return cantAsientos;
+	}
+
+	public void setCantAsientos(String cantAsientos) {
+		this.cantAsientos = cantAsientos;
+	}
+
 
 }
--- src/main/java/tuti/desi/entidades/Vuelo.java
+++ src/main/java/tuti/desi/entidades/Vuelo.java
@@ -1,5 +1,96 @@
 package tuti.desi.entidades;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+
+@Entity
 public class Vuelo {
 
+	@Id
+	private Long nroVuelo;
+	
+	private String tipoVuelo;
+	
+	private Date fechaVuelo;
+	
+	private String horaVuelo;
+	
+	@ManyToOne
+	private Avion avion;
+	
+	@ManyToOne
+	private Ciudad origen;
+	
+	@ManyToOne
+	private Ciudad destino;
+	
+	private String estado = "normal";
+
+	public String getEstado() {
+		return estado;
+	}
+
+
+	public Long getNroVuelo() {
+		return nroVuelo;
+	}
+
+	public void setNroVuelo(Long nroVuelo) {
+		this.nroVuelo = nroVuelo;
+	}
+
+	public String getTipoVuelo() {
+		return tipoVuelo;
+	}
+
+	public void setTipoVuelo(String tipoVuelo) {
+		this.tipoVuelo = tipoVuelo;
+	}
+
+	public Date getFechaVuelo() {
+		return fechaVuelo;
+	}
+
+	public void setFechaVuelo(Date fechaVuelo) {
+		this.fechaVuelo = fechaVuelo;
+	}
+
+	public String getHoraVuelo() {
+		return horaVuelo;
+	}
+
+	public void setHoraVuelo(String horaVuelo) {
+		this.horaVuelo = horaVuelo;
+	}
+
+	public Avion getAvion() {
+		return avion;
+	}
+
+	public void setAvion(Avion avion) {
+		this.avion = avion;
+	}
+
+	public Ciudad getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(Ciudad origen) {
+		this.origen = origen;
+	}
+
+	public Ciudad getDestino() {
+		return destino;
+	}
+
+	public void setDestino(Ciudad destino) {
+		this.destino = destino;
+	}
+	
+	
+	
 }
